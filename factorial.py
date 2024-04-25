833df09ceaa4b963729754d1af0b14a8da696663
Vincent Roessle <vincent.roessle@gmail.com>
2024-04-25 14:19:11 +0200

    factorial python

--- factorial.py
+++ factorial.py
@@ -4,6 +4,8 @@ def factorial(n):
     result = 1
     while n > 1:
         result *= n
+        n = n - 1
     return result
 
-factorial(int(sys.argv[1]))
+f = factorial(int(sys.argv[1]))
+print(f)
