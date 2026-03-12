a75ce4943e748d9553cfe20dbae45ac2e3da2409
mvprkk-dot <limurad42@gmail.com>
2026-03-12 22:33:24 +0400

    Update factorial_recursive.py

--- factorial_recursive.py
+++ factorial_recursive.py
@@ -2,10 +2,13 @@
 import sys
 
 def factorial(n):
+    
     if n == 0:
         return 1
     else:
-        return n * factorial(n-1)
+        return n * factorial(n - 1)
 
-f = factorial(int(sys.argv[1]))
-print(f)
+if __name__ == "__main__":
+    # Terminal arqumentindən ədədi götürüb integer-ə çeviririk
+    f = factorial(int(sys.argv[1]))
+    print(f)
